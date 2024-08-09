c4778740155ac05a077d4aa373909ec57b5b52f4
seraprogrammer1 <165708692+seraprogrammer1@users.noreply.github.com>
2024-08-09 15:00:09 -0400

    update 02
    added  env file for tutorial use

--- No_React/server.js
+++ No_React/server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const path = require("path");
+require("dotenv").config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 4000;
 
 // Serve static files
 app.use(express.static(path.join(__dirname, "public")));
